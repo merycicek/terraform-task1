e46fcc3b232188fb45b9083ee902104fa1ae3eef
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 16:23:19 +0000

    added security group file

---
+++ security_group_task1.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "task1_terraform1"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-007235786ca81350c"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "task1_terraform1"
+  }
+}
