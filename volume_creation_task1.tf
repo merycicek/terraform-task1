85cac3df803dcfbd39c4ca2dbf0f1c7686c08247
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 18:23:50 +0000

    create volume

--- volume_creation_task1.tf
+++ volume_creation_task1.tf
@@ -1,7 +1,8 @@
-resource "aws_ebs" "goodmorning1" {
-  device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs.goodmorning1.name}"
-  instance_id = "ami-0d6621c01e8c2de2c"
+resource "aws_ebs_volume" "myvolume" {
   availability_zone = "us-west-2a"
   size              = 100
+
+  tags = {
+    Name = "HelloWorld1"
+  }
 }
