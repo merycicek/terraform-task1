271375fb4285daa2ac1d71fe95644b9c4baafb83
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 18:13:40 +0000

    fixing resources

--- volume_creation_task1.tf
+++ volume_creation_task1.tf
@@ -2,19 +2,6 @@ resource "aws_volume_attachment" "volue_task1" {
   device_name = "/dev/sdh"
   volume_id   = "${aws_volume_attachment.volue_task1.id}"
   instance_id = "ami-0d6621c01e8c2de2c"
-}
-
-resource "aws_instance" "web" {
-  ami               = "ami-0d6621c01e8c2de2c"
-  availability_zone = "us-west-2a"
-  instance_type     = "t2.large"
-
-  tags = {
-    Name = "HelloWorld1"
-  }
-}
-
-resource "aws_volume_attachment" "volue_task1" {
   availability_zone = "us-west-2a"
   size              = 100
 }
