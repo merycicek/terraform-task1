610588eacbae6cd2f446f816bfbd1e59d413428a
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 16:13:51 +0000

    added changes

--- key_Pair_task1.tf
+++ key_Pair_task1.tf
@@ -1,5 +1,6 @@
 resource "aws_key_pair" "us-west-2-key.tf" {
   key_name   = "terraform-bastion"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD+qN+JF+P6gVqQB1Pq6ZkGT2/Q+gdFshMmNiCDUkWgw18NbXZJ+t5uICaEawyZhLsf2MSCEm03JBNfSeMTje1GnxzhKNKKVHkQ4Pm3GsQeuFfZMVRrB2WhzSkC7vq9aGp3LBvug5+QBpEhdy1d6MqDjpOjaxlbZwpRRfteKsuFVjKWCmm89XvhGBFl9ZtV7MuJODyXPxHNRlB0uXoiOeV2Q8UQQUZMoR0HymJAvN/3944caO2kKmwxZIXyA+xdmOLqtWvYhfX6rEmmcs5JbrmukzhKJFIW9/m+R0DvXU2Yiz50cEaOlctbLut3e7lvhDbiMTwnt81Qrsd4avxtCXoB ec2-user@ip-172-31-28-39.eu-west-2.compute.internal
-"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+
+  
 }
