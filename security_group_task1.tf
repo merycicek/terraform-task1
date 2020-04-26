c220bdbc7597b14a60ca76c381dc78a62a39cd35
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 16:31:45 +0000

    }} error

--- security_group_task1.tf
+++ security_group_task1.tf
@@ -2,7 +2,7 @@ resource "aws_security_group" "allow_tls" {
   name        = "task1_terraform1"
   description = "Allow TLS inbound traffic"
   vpc_id      = "vpc-007235786ca81350c"
-
+}
   ingress {
     description = "TLS from VPC"
     from_port   = 22
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_tls" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -36,4 +36,4 @@ resource "aws_security_group" "allow_tls" {
     Name = "task1_terraform1"
   }
   
-  }
+
