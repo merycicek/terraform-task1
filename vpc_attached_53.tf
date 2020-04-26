df9160c22fbaf4a9aac8962b7a30321bbd4b93ff
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 22:22:44 +0000

    remove cdir

--- vpc_attached_53.tf
+++ vpc_attached_53.tf
@@ -1,14 +1,14 @@
-resource "aws_vpc" "primary" {
-cidr_block           = "10.6.0.0/16"
-enable_dns_hostnames = true
-enable_dns_support   = true
-}
+# resource "aws_vpc" "primary" {
+# cidr_block           = "10.6.0.0/16"
+# enable_dns_hostnames = true
+# enable_dns_support   = true
+# }
 
-resource "aws_vpc" "secondary" {
-cidr_block           = "10.7.0.0/16"
-enable_dns_hostnames = true
-enable_dns_support   = true
-}
+# resource "aws_vpc" "secondary" {
+# cidr_block           = "10.7.0.0/16"
+# enable_dns_hostnames = true
+# enable_dns_support   = true
+# }
 
 # resource "aws_route53_record" "web" {
 #   zone_id = "${aws_route53_zone.web.zone_id}"
