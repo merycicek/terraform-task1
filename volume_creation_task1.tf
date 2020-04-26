dc336880640abd874444c180cf3e1c6b5d4dfe00
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 18:11:04 +0000

    added ...

--- volume_creation_task1.tf
+++ volume_creation_task1.tf
@@ -1,11 +1,11 @@
 resource "aws_volume_attachment" "volue_task1" {
   device_name = "/dev/sdh"
   volume_id   = "${aws_volume_attachment.volue_task1.id}"
-  instance_id = "${aws_instance.web.id}"
+  instance_id = "ami-0d6621c01e8c2de2c"
 }
 
 resource "aws_instance" "web" {
-  ami               = "ami-21f78e11"
+  ami               = "ami-0d6621c01e8c2de2c"
   availability_zone = "us-west-2a"
   instance_type     = "t2.large"
 
