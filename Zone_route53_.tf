cd6347bd86633a73eba6d8585cd28536457da1a6
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 19:27:53 +0000

    added changes

--- Zone_route53_.tf
+++ Zone_route53_.tf
@@ -1,25 +1,25 @@
 resource "aws_route53_zone" "mymain" {
-  name = "merycicek.com"
+  name = "yourdomain.com"
 }
 
-resource "aws_route53_zone" "mydomain" {
-  name = "mydomain.merycicek.com"
+resource "aws_route53_zone" "yourdomain" {
+  name = "www.yourdomain.com"
 
   tags = {
-    Environment = "mydomain"
+    Environment = "yourdomain"
   }
 }
 
-resource "aws_route53_record" "mydomain-ns" {
+resource "aws_route53_record" "yourdomain-ns" {
   zone_id = "${aws_route53_zone.mymain.zone_id}"
-  name    = "mydomain.merycicek.com"
+  name    = "yourdomain.merycicek.com"
   type    = "NS"
   ttl     = "30"
 
   records = [
-    "${aws_route53_zone.mydomain.name_servers.0}",
-    "${aws_route53_zone.mydomain.name_servers.1}",
-    "${aws_route53_zone.mydomain.name_servers.2}",
-    "${aws_route53_zone.mydomain.name_servers.3}",
+    "${aws_route53_zone.yourdomain.name_servers.0}",
+    "${aws_route53_zone.yourdomain.name_servers.1}",
+    "${aws_route53_zone.yourdomain.name_servers.2}",
+    "${aws_route53_zone.yourdomain.name_servers.3}",
   ]
 }
