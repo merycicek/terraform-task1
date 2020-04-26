76e06cc91eb71154a747ee0f7c55a0943a8cc674
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 20:04:25 +0000

    remove file

--- Zone_route53_.tf
+++ Zone_route53_.tf
@@ -1,25 +1,6 @@
-resource "aws_route53_zone" "mymain" {
-  name = "yourdomain.com"
-}
 
-resource "aws_route53_zone" "yourdomain" {
-  name = "www.yourdomain.com"
 
-  tags = {
-    Environment = "yourdomain"
-  }
-}
+ 
 
-resource "aws_route53_record" "yourdomain-ns" {
-  zone_id = "${aws_route53_zone.mymain.zone_id}"
-  name    = "yourdomain.merycicek.com"
-  type    = "NS"
-  ttl     = "30"
 
-  records = [
-    "${aws_route53_zone.yourdomain.name_servers.0}",
-    "${aws_route53_zone.yourdomain.name_servers.1}",
-    "${aws_route53_zone.yourdomain.name_servers.2}",
-    "${aws_route53_zone.yourdomain.name_servers.3}",
-  ]
 }
