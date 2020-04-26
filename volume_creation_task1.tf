6ccaa105c2ac912e0686383b7c0646144bca4896
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 17:39:39 +0000

    fixed repeated resourse

--- volume_creation_task1.tf
+++ volume_creation_task1.tf
@@ -3,9 +3,6 @@ resource "aws_ebs_volume" "web" {
   device_name = "/dev/sdh"
   volume_id   = "${aws_ebs_volume.web.id}"
   instance_id = "${aws_instance.web.id}"
-}
-
-resource "aws_ebs_volume" "web" {
   availability_zone = "us-west-2a"
   size              = 100
 }
