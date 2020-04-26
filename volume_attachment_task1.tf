b08898224dce65e4e1bffc0bfb19eda820eca6ac
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 18:40:03 +0000

    deleting multible resources

--- volume_attachment_task1.tf
+++ volume_attachment_task1.tf
@@ -3,18 +3,3 @@ resource "aws_volume_attachment" "myvolume" {
   volume_id   = "${aws_ebs_volume.myvolume.id}"
   instance_id = "${aws_instance.web.id}"
 }
-
-resource "aws_instance" "web" {
-  ami               = "ami-0d6621c01e8c2de2c"
-  availability_zone = "us-west-2a"
-  instance_type     = "t2.large"
-
-  tags = {
-    Name = "HelloWorld1"
-  }
-}
-
-resource "aws_ebs_volume" "myvolume" {
-  availability_zone = "us-west-2a"
-  size              = 100
-}
