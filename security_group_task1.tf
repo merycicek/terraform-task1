9a61013b48a0a06e921079d32a7b9f3d589b94b1
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 16:34:05 +0000

    } error

--- security_group_task1.tf
+++ security_group_task1.tf
@@ -2,7 +2,7 @@ resource "aws_security_group" "allow_tls" {
   name        = "task1_terraform1"
   description = "Allow TLS inbound traffic"
   vpc_id      = "vpc-007235786ca81350c"
-}
+
   ingress {
     description = "TLS from VPC"
     from_port   = 22
@@ -35,5 +35,5 @@ resource "aws_security_group" "allow_tls" {
   tags = {
     Name = "task1_terraform1"
   }
-  
+}  
 
