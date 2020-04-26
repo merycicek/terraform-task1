c3c0e8625aae9185ae2fb7f0bdcbf1931efc19ea
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-04-26 18:07:27 +0000

    added Interpolation

--- volume_creation_task1.tf
+++ volume_creation_task1.tf
@@ -1,8 +1,20 @@
-
-resource "aws_ebs_volume" "web1" {
+resource "aws_volume_attachment" "volue_task1" {
   device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs_volume.web1.id}"
+  volume_id   = "${aws_volume_attachment.volue_task1.id}"
   instance_id = "${aws_instance.web.id}"
+}
+
+resource "aws_instance" "web" {
+  ami               = "ami-21f78e11"
+  availability_zone = "us-west-2a"
+  instance_type     = "t2.large"
+
+  tags = {
+    Name = "HelloWorld1"
+  }
+}
+
+resource "aws_volume_attachment" "volue_task1" {
   availability_zone = "us-west-2a"
   size              = 100
 }
